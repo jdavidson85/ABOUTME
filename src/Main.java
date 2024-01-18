c45202b5419d6df36c0f6f5846e63e66109de27e
Joe Davidson <jdavidson@students.mchenry.edu>
2024-01-17 23:00:08 -0600

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,13 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            System.out.println("My name is Joe Davidson");
+            System.out.println("I love programming and learning new coding languages");
+            System.out.println("I play video games in my spare time");
+            System.out.println("I am a father of two boys");
         }
     }
 }
